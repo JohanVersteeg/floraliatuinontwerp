ecca70ee4af9327094586ba9a260c79aa49c52fc
Johan Versteeg <johan.versteeg@nowonlineutrecht.nl>
2016-01-04 14:45:05 +0100

    Added contact info

--- angular/slick-directive.js
+++ angular/slick-directive.js
@@ -6,6 +6,7 @@ angular.module("app").directive('slick', function() {
             for(var i = 0; i < images.length; i++){
                 $(element).append("<img src='"+images[i]+"' />");
             }
+            
             $(element).slick(scope.$eval(attrs.slick));
             $(element).slick("slickGoTo", 0);
             $(element).css("opacity",1);
