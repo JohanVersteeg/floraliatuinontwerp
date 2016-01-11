15cee06f679f445ceb8870b553a54b12d8781578
Johan Versteeg <johan.versteeg@nowonlineutrecht.nl>
2016-01-11 09:01:35 +0100

    Add fancybox

--- angular/portfolio-controller.js
+++ angular/portfolio-controller.js
@@ -1,19 +1,37 @@
 angular.module("app")
-	.controller("PortfolioController", PortfolioController);
+    .controller("PortfolioController", PortfolioController);
 
-PortfolioController.$inject = ['$scope','$http'];
+PortfolioController.$inject = ['$scope', '$http'];
 
-function PortfolioController($scope,$http) {		
-	$http.get("config/portfolio.json").then(function(response){
-		$scope.content = response.data;   
-	});
-    
-    $http.get("config/portfolio-items.json").then(function(response){
-		$scope.items = response.data;   
-	});
-	
-	$scope.visibleItems = 6;
-	$scope.showMore = function(){
-		$scope.visibleItems += 3;
-	}
+function PortfolioController($scope, $http) {
+    $http.get("config/portfolio.json").then(function (response) {
+        $scope.content = response.data;
+    });
+
+    $http.get("config/portfolio-items.json").then(function (response) {
+        $scope.items = response.data;
+    });
+
+    $scope.visibleItems = 6;
+    $scope.showMore = function () {
+        $scope.visibleItems += 3;
+    }
+
+
+    $scope.openLightBox = function (item) {
+        var fotos = [];
+        for (var i = 0; i < item.fotos.length; i++) {
+            fotos.push({ href: item.fotos[i] });
+        }
+        $.fancybox.open(fotos, {
+            padding: 0,
+            helpers: {
+                thumbs: {
+                    width: 100,
+                    height: 100
+                }
+            },
+            closeBtn: false
+        });
+    }
 };
