9e4f3ccfedbe89fa8388fb454198b328eddf04b0
Johan Versteeg <johan.versteeg@nowonlineutrecht.nl>
2016-01-16 18:16:09 +0100

    Done

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,9 +14,18 @@
     $message = nl2br($_POST['message']);
         
     // Create the email and send the message
-    $to = 'versteeg.joh@outlook.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $to = 'info@floralia-tuinontwerp.nl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
     $email_subject = "Website Contact Form: $name";
     $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-    $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-    $headers .= "Reply-To: $email_address";	
-    mail($to,$email_subject,$email_body,$headers);
+	$headers = "MIME-Version: 1.0\n";
+	$headers .= "Content-type: text/html; charset=iso-8859-1\n"; 
+	$headers .= "From: no-reply@floralia-tuinontwerp.nl\n";
+    
+    if(mail($to,$email_subject,$email_body,$headers))
+    {
+        echo "Done";
+    }
+    else
+    {
+        echo "Fail";
+    }
