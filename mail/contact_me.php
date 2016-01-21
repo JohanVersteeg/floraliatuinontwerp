633a09f22affd4a64ebc2244a5bac0f77ee1992e
Johan Versteeg <johan.versteeg@nowonlineutrecht.nl>
2016-01-21 15:02:36 +0100

    Last changes

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@
     // Create the email and send the message
     $to = 'info@floralia-tuinontwerp.nl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
     $email_subject = "Website Contact Form: $name";
-    $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+    $email_body = "<h2>Bericht van de website</h2><b>Naam:</b> $name<br /><b>E-mail:</b> $email_address<br /><b>Telefoon:</b> $phone<br /><b>Bericht:</b><br />$message";
 	$headers = "MIME-Version: 1.0\n";
 	$headers .= "Content-type: text/html; charset=iso-8859-1\n"; 
 	$headers .= "From: no-reply@floralia-tuinontwerp.nl\n";
